54578fb71af259c201c1599f9c90e2012921bde0
Arman Shehad <65397811+armanshehad@users.noreply.github.com>
2020-05-27 17:56:08 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    // click event on toggle menu
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+
+
+});
+
+
+
+
+
+
